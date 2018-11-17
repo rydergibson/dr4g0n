3d0026b12b58589e7d0d22f7cafe4c0d3b6e5c65
rydergibson <rydergibson1227@gmail.com>
2018-11-17 00:42:58 -0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 echo "WARNING: Root access may be needed."
 pip install requests
 pip3 install requests
-sudo echo "alias dragon='python ~/Downloads/dr4g0n/main.py'" >> ~/.bashrc
 read -p "Installation finished! Press Enter to exit."
-source ~/.bashrc
-echo "Enter this command: \"sudo echo "alias dragon='python ~/Downloads/dr4g0n/main.py'" >> ~/.bashrc\""
+echo "Enter this command: \"sudo echo "alias dragon='python ~/Downloads/dr4g0n/main.py'" >> ~/.bashrc;source ~/.bashrc\""
